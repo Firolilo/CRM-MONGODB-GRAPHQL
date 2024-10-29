c1fa502d8b273a5c78d614f97f9f88314f26f33d
Firolilo <brunofioriloar@gmail.com>
2024-10-29 11:36:58 -0400

    nuevoUsuario

--- db/resolvers.js
+++ db/resolvers.js
@@ -1,6 +1,11 @@
 const resolvers = {
     Query: {
         obtenerCurso:()=>'Bienvenido Estudiante de Base de Datos III',
+    },
+    Mutation: {
+        nuevoUsuario:async (_,{input}) => {
+            console.log(input);
+        }
     }
 }
 module.exports = resolvers;
--- db/schemas.js
+++ db/schemas.js
@@ -5,5 +5,24 @@ const typeDefs = gql`
 type Query {
     obtenerCurso:String
 }
+
+type Usuario {
+    id: ID
+    nombre: String
+    apellido: String
+    email: String
+    creado: String
+}
+
+input inputUsuario{
+    nombre: String
+    apellido: String
+    email: String
+    password: String
+}
+
+type Mutation {
+    nuevoUsuario(input:inputUsuario) : Usuario
+}
 `;
 module.exports = typeDefs;
